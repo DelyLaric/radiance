f76b679d2b48218390b7f284961d45dbc7012240
zhanglan <delylaric@gmail.com>
2018-10-02 14:29:23 +0800

    chore: add margin-bottom  for VueCodeDemo

--- docs/.vuepress/components/VueCodeDemo.vue
+++ docs/.vuepress/components/VueCodeDemo.vue
@@ -66,6 +66,7 @@ export default {
 .demo-container
   padding: 0
   margin-top: 1rem
+  margin-bottom: 1rem
   border-radius: 6px
   box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1)
 
